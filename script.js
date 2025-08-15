61875d4b6ef07b1cfe7b308b1a51bd50bbfce82e
mateuzinhoww <matheus.macedo.castro@escola.pr.gov.br>
2025-08-15 08:39:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const avanca = document.querySelectorAll('.btn-proximo');
 avanca.forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+        const proximoPasso = 'passo-' + this.getAttribute('next-date');
 
         atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo');
+        document.getElementById(nextstep).classList.add('active');
     })
 })
